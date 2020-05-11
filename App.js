40190f0693670e1ed37a228d662c899811e389a6
Jin <jinpark1504@gmail.com>
2020-05-11 19:31:03 -0400

    map using expo

--- App.js
+++ App.js
@@ -1,19 +1,83 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import MapView, {Marker} from 'react-native-maps';
+import * as Permissions from 'expo-permissions';
+import * as Location from 'expo-location';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    location: null,
+  }
+
+  _getLocationAsync = async () => {
+    // let {status} = await Permissions.askAsync(Permissions.LOCATION);
+    // if (status !== 'granted') {
+    //   console.error('Location is not granted!')
+    //   return;
+    // }
+    let {status} = await Location.requestPermissionsAsync();
+
+    if (status !== 'granted') {
+      console.error('Permission to access location was denied')
+      return
+    }
+
+    let location = await Location.getCurrentPositionAsync({})
+
+    let therapy = (await Location.geocodeAsync('574 Carlton St'))[0]
+    let timHortons = (await Location.geocodeAsync('579 Carlton St'))[0]
+    let carletonPS = (await Location.geocodeAsync('1 Carlton Park Dr'))[0]
+    let where = (await Location.reverseGeocodeAsync(location.coords))[0];
+    this.setState({
+      location, 
+      places: {
+        therapy, timHortons, carletonPS
+      },
+      where,
+    })
+
+  }
+
+  componentDidMount() {
+    this._getLocationAsync()
+  }
+
+  render() {
+      if (!this.state.location) return (<View />);
+      return (
+      <MapView 
+        style={{flex:1}}
+        initialRegion={{
+          latitude: this.state.location.coords.latitude,
+          longitude: this.state.location.coords.longitude,
+          latitudeDelta: 0.0922 / 5,
+          longitudeDelta: 0.0421 / 5,
+        }}
+      >
+        <Marker 
+          coordinate={this.state.location.coords}
+          title="You are here"
+          description={this.state.where.name}
+        />
+        <Marker
+          coordinate={this.state.places.therapy}
+          title="Therapy"
+          description="Cornerston Therapy and Wellness"
+          pinColor="blue"
+        />
+        <Marker
+          coordinate={this.state.places.timHortons}
+          title="Tim Hortons"
+          description="Coffee chain serving snacks and light fare"
+          pinColor="yellow"
+        />
+        <Marker
+          coordinate={this.state.places.carletonPS}
+          title="Carleton Public School"
+          pinColor="green"
+        />
+      </MapView>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
